baa1ee0b36a5db13f891de67aab52bd564631d99
Nguyen Quoc Dat <dnguyenquoc45@gmail.com>
2025-09-19 08:22:48 +0700

    fix conflict

--- config.php
+++ config.php
@@ -6,19 +6,19 @@ const _ACTION = 'dashboard';
 const _CODE = true;
 
 // thiết lập host
-define('_WEB_HOST', 'http://' . $_SERVER['HTTP_HOST'] . '/zzzzzz_btl/zzzzz_btl');
+define('_WEB_HOST', 'http://' . $_SERVER['HTTP_HOST'] . '/project_finish');
 define('_WEB_HOST_TEMPLATES', _WEB_HOST . '/templates');
 
 // thiết lập path
 define('_WEB_PATH', __DIR__);
 define('_WEB_PATH_TEMPLATES', _WEB_PATH . '\\templates');
 
-define('_IMGA_', '/zzzzzz_btl/zzzzz_btl/templates/image/avatars/');
-define('_IMGB_', '/zzzzzz_btl/zzzzz_btl/templates/image/banner/');
-define('_IMGC_', '/zzzzzz_btl/zzzzz_btl/templates/image/categories/');
-define('_IMGG_', '/zzzzzz_btl/zzzzz_btl/templates/image/gift/');
-define('_IMGP_', '/zzzzzz_btl/zzzzz_btl/templates/image/products/');
-define('_IMGCS_', '/zzzzzz_btl/zzzzz_btl/templates/image/codes/');
+define('_IMGA_', '/project_finish/templates/image/avatars/');
+define('_IMGB_', '/project_finish/templates/image/banner/');
+define('_IMGC_', '/project_finish/templates/image/categories/');
+define('_IMGG_', '/project_finish/templates/image/gift/');
+define('_IMGP_', '/project_finish/templates/image/products/');
+define('_IMGCS_', '/project_finish/templates/image/codes/');
 
 // database
 const _HOST = "localhost";
