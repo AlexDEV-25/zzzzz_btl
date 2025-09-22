b65db26556c475cabe7e9602a98c26fe955cff0e
alex <hoangvanduc2504@gmail.com>
2025-09-22 10:36:24 +0700

    sua cancelbill

--- modules/billsCustom/cancelBill.php
+++ modules/billsCustom/cancelBill.php
@@ -6,17 +6,44 @@ if (!defined('_CODE')) {
 $filterAll = filter();
 $billId = $filterAll['billId'] ?? null;
 $userId = $filterAll['userId'] ?? null;
+$billStatus = selectOne("SELECT * FROM bills WHERE id = $billId")['status'];
 
 if ($billId) {
-    $sql = "UPDATE bills 
-    SET status = -1 
-    WHERE id = $billId AND status IN (0,1,2)";
-    if (query($sql)) {
-        setFlashData('msg', '✅ Hủy đơn thành công!');
-        setFlashData('msg_type', 'success');
-    } else {
-        setFlashData('msg', '❌ Hủy đơn thất bại!');
-        setFlashData('msg_type', 'danger');
+    if ($status == 0) {
+        $sql = "UPDATE bills 
+        SET status = -1 
+        WHERE id = $billId AND status IN (0)";
+        if (query($sql)) {
+            setFlashData('msg', '✅ Hủy đơn thành công!');
+            setFlashData('msg_type', 'success');
+        } else {
+            setFlashData('msg', '❌ Hủy đơn thất bại!');
+            setFlashData('msg_type', 'danger');
+        }
+    } elseif ($status == 1 || $status == 2) {
+        $sql = "UPDATE bills 
+        SET status = -1 
+        WHERE id = $billId AND status IN (1,2)";
+        if (query($sql)) {
+            setFlashData('msg', '✅ Hủy đơn thành công!');
+            setFlashData('msg_type', 'success');
+        } else {
+            setFlashData('msg', '❌ Hủy đơn thất bại!');
+            setFlashData('msg_type', 'danger');
+        }
+
+        // ✅ Hủy đơn → cộng lại số lượng
+        $listBillDetail = selectAll("SELECT * FROM products_bill WHERE id_bill = $billId");
+
+        foreach ($listBillDetail as $item) {
+            $productDetailId = (int) $item['id_product_detail'];
+            $productDetail   = selectOne("SELECT amount FROM products_detail WHERE id = $productDetailId");
+
+            if ($productDetail) {
+                $newAmount = $productDetail['amount'] + $item['amount_buy'];
+                update('products_detail', ['amount' => $newAmount], "id = $productDetailId");
+            }
+        }
     }
 }
 redirect("?module=billsCustom&action=customBillDetail&role=0&userId=$userId&billId=$billId");
